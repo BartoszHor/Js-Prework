f5699847117224cc7754c5ea16819cbcd2c7ed3d
Bartosz Horoba <bartosz.horoba@gmail.com>
2020-07-02 17:56:47 +0200

    Add random computer move

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
-let playerMove = "papier"
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-let computerMove = `kamień`;
+console.log('Wylosowana liczba to: ' + randomNumber);
 
-printMessage("Mój ruch to " + playerMove + ". Jeśli Twój ruch to" + computerMove + "to przegrywasz")
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+} else if (randomNumber == 2) {
+  computerMove = "papier"
+} else if (randomNumber == 3) {
+  computerMove = "nożyce"
+}
+
+printMessage('Ruch komputera to: ' + computerMove);
+
+
+
+
