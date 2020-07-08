6bfd22a7919f9f7da4c9a246c3b5c4171418173b
Bartosz Horoba <bartosz.horoba@gmail.com>
2020-07-08 12:51:08 +0200

    Meet JS requirements in .js file

--- js/script.js
+++ js/script.js
@@ -1,89 +1,87 @@
 'use strict';
-let rock = document.getElementById("rock");
-let paper = document.getElementById("paper");
-let scissors = document.getElementById("scissors");
+{
+	function playGame(playerInput) {
 
-function playGame(playerInput) {
+	clearMessages("messages");
 
-	clearMessages("messages")
-
-let randomNumber = Math.floor(Math.random() * 3 + 1);
-
-let computerMove = getMoveName(randomNumber)
-
-let playerMove = getMoveName(playerInput);
+	const randomNumber = Math.floor(Math.random() * 3 + 1);
+	const computerMove = getMoveName(randomNumber);
+	const playerMove = getMoveName(playerInput);
 
 	function getMoveName(argMoveId){
-	  if(argMoveId == 1){
-	    return 'kamień';
-	  } else if (argMoveId == 2){
-	  	return 'papier';
-	  } else if (argMoveId == 3){
-	  	return 'nożyce'
-	  } else {
-	    printMessage("messages", 'Nie znam ruchu o id ' + argMoveId + '.');
-	    return 'nieznany ruch';
-	  }
-	}
+		if(argMoveId == 1){
+		    return 'kamień';
+		} else if (argMoveId == 2){
+		  	return 'papier';
+		} else if (argMoveId == 3){
+		  	return 'nożyce'
+		} else {
+			printMessage("messages", 'Nie znam ruchu o id ' + argMoveId + '.');
+			return 'nieznany ruch';
+			}
+		}
 
 	printMessage("messages", 'Ruch komputera to: ' + computerMove);
-
 	printMessage("messages", 'Ruch gracza to: ' + playerMove);
 
-	let result = displayResult(computerMove, playerMove);
+	const result = displayResult(computerMove, playerMove);
 
-	function displayResult(x, y) {
-		console.log("Test")
-		console.log(x, y)
-	if((x == "kamień" && y == "papier") || (x == "papier" && y == "nożyce") || (x == "nożyce" && y == "kamień")) {
-	   printMessage("messages", 'Wygrana gracza')
-	   playerWins++
-	   clearMessages("result");
-	   printMessage("result","Wynik gracza: " + playerWins)
-	   printMessage("result", "Wynik komputera: " + computerWins)
-	} else if (x === y) {
-		printMessage("messages", 'Remis');
-	} else {
-		printMessage("messages", 'Wygrana komputera');
-		computerWins++;
-		clearMessages("result");
-		printMessage("result", "Wynik gracza: " + playerWins)
-		printMessage("result", "Wynik komputera: " + computerWins)
+		function displayResult(x, y) {
+			console.log("Test")
+			console.log(x, y)
+		if((x == "kamień" && y == "papier") || (x == "papier" && y == "nożyce") || (x == "nożyce" && y == "kamień")) {
+		   printMessage("messages", 'Wygrana gracza')
+		   playerWins++
+		   clearMessages("result");
+		   printMessage("result","Wynik gracza: " + playerWins)
+		   printMessage("result", "Wynik komputera: " + computerWins)
+		} else if (x === y) {
+			printMessage("messages", 'Remis');
+		} else {
+			printMessage("messages", 'Wygrana komputera');
+			computerWins++;
+			clearMessages("result");
+			printMessage("result", "Wynik gracza: " + playerWins)
+			printMessage("result", "Wynik komputera: " + computerWins)
+			}
+		}
+	} 
+
+	let computerWins = 0;
+	let playerWins = 0;
+
+	function rerunGame(){		
+		if ((computerWins > 4) || (playerWins > 4)) {
+			clearMessages("result") 
+			printMessage("result", "Wynik gracza: " + playerWins)
+			printMessage("result", "Wynik komputera: " + computerWins)
+			console.log (playerWins, computerWins);
+			if (computerWins == 5) {
+				clearMessages("result");
+				printMessage("result", "Wygrywa komputer");
+			} else {
+				clearMessages("result");
+				printMessage("result", "Wygrywa gracz");
+			} computerWins = 0;
+			  playerWins = 0;
 		}
 	}
-} 
 
-rock.addEventListener("click", function(){
-   playGame(1)
-   rerunGame()
-})
-paper.addEventListener("click", function(){
-  playGame(2)
-  rerunGame()
-})
-scissors.addEventListener("click", function(){
-  playGame(3)
-  rerunGame()
-})
+	const rock = document.getElementById("rock"),
+	paper = document.getElementById("paper"),
+	scissors = document.getElementById("scissors");
 
-let computerWins = 0;
-let playerWins = 0;
-
-function rerunGame(){
-	if ((computerWins > 4) || (playerWins > 4)) {
-		clearMessages("result") 
-		printMessage("result", "Wynik gracza: " + playerWins)
-		printMessage("result", "Wynik komputera: " + computerWins)
-		console.log (playerWins, computerWins);
-		if (computerWins == 5) {
-			clearMessages("result");
-			printMessage("result", "Wygrywa komputer");
-		} else {
-			clearMessages("result");
-			printMessage("result", "Wygrywa gracz");
-		} computerWins = 0;
-		  playerWins = 0;
-	}
+	rock.addEventListener("click", function(){
+	   playGame(1)
+	   rerunGame()
+	})
+	paper.addEventListener("click", function(){
+	  playGame(2)
+	  rerunGame()
+	})
+	scissors.addEventListener("click", function(){
+	  playGame(3)
+	  rerunGame()
+	})
 }
-
 
